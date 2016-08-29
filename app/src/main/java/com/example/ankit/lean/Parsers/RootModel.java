98b209e24de3f7908ac18d0dd45cfdcf2021162c
jiowifi <xutephone16@gmail.com>
2016-08-29 22:05:05 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public String organizer_name;
-	public String privacy;
-	public String name;
-	public int id;
-	public String email;
-	public String type;
-	public String logo;
-	public String description;
-	public String start_time;
-	public String topic;
-	public String schedule_published_on;
-	public String background_image;
-	public String timezone;
-	public Call_for_papersModel _call_for_papers;
-	public String state;
-	public String organizer_description;
-	public String location_name;
-	public CreatorModel _creator;
-	public ArrayList<Social_linksModel> social_links;
-	public String code_of_conduct;
-	public CopyrightModel _copyright;
-	public VersionModel _version;
 	public String end_time;
+	public int id;
+	public String code_of_conduct;
+	public String organizer_name;
+	public String topic;
+	public String state;
+	public String privacy;
+	public String schedule_published_on;
+	public String email;
+	public String start_time;
+	public String type;
+	public String location_name;
+	public String description;
+	public String logo;
+	public String name;
+	public Call_for_papersModel _call_for_papers;
+	public String timezone;
+	public String background_image;
+	public String organizer_description;
+	public CopyrightModel _copyright;
+	public ArrayList<Social_linksModel> social_links;
+	public VersionModel _version;
+	public CreatorModel _creator;
 
-	public RootModel(String organizer_name, String privacy, String name, int id, String email, String type, String logo, String description, String start_time, String topic, String schedule_published_on, String background_image, String timezone, Call_for_papersModel call_for_papers, String state, String organizer_description, String location_name, CreatorModel creator, ArrayList<Social_linksModel> social_links, String code_of_conduct, CopyrightModel copyright, VersionModel version, String end_time) {
+	public RootModel(String end_time, int id, String code_of_conduct, String organizer_name, String topic, String state, String privacy, String schedule_published_on, String email, String start_time, String type, String location_name, String description, String logo, String name, Call_for_papersModel call_for_papers, String timezone, String background_image, String organizer_description, CopyrightModel copyright, ArrayList<Social_linksModel> social_links, VersionModel version, CreatorModel creator) {
 
-		this.organizer_name = organizer_name;
-		this.privacy = privacy;
-		this.name = name;
-		this.id = id;
-		this.email = email;
-		this.type = type;
-		this.logo = logo;
-		this.description = description;
-		this.start_time = start_time;
-		this.topic = topic;
-		this.schedule_published_on = schedule_published_on;
-		this.background_image = background_image;
-		this.timezone = timezone;
-		this._call_for_papers = call_for_papers;
-		this.state = state;
-		this.organizer_description = organizer_description;
-		this.location_name = location_name;
-		this._creator = creator;
-		this.social_links = social_links;
-		this.code_of_conduct = code_of_conduct;
-		this._copyright = copyright;
-		this._version = version;
 		this.end_time = end_time;
+		this.id = id;
+		this.code_of_conduct = code_of_conduct;
+		this.organizer_name = organizer_name;
+		this.topic = topic;
+		this.state = state;
+		this.privacy = privacy;
+		this.schedule_published_on = schedule_published_on;
+		this.email = email;
+		this.start_time = start_time;
+		this.type = type;
+		this.location_name = location_name;
+		this.description = description;
+		this.logo = logo;
+		this.name = name;
+		this._call_for_papers = call_for_papers;
+		this.timezone = timezone;
+		this.background_image = background_image;
+		this.organizer_description = organizer_description;
+		this._copyright = copyright;
+		this.social_links = social_links;
+		this._version = version;
+		this._creator = creator;
 
 	}
 
