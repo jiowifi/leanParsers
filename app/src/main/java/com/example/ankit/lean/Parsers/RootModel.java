b2026af0f3ea980cb594efed8673880b2b07ce01
jiowifi <xutephone16@gmail.com>
2016-08-29 22:14:16 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public String email;
-	public Call_for_papersModel _call_for_papers;
-	public String state;
-	public CopyrightModel _copyright;
-	public CreatorModel _creator;
-	public String type;
-	public String start_time;
-	public ArrayList<Social_linksModel> social_links;
-	public String timezone;
-	public String location_name;
-	public String topic;
 	public VersionModel _version;
-	public String name;
 	public String code_of_conduct;
-	public String organizer_name;
+	public String name;
+	public String end_time;
+	public int id;
 	public String logo;
 	public String privacy;
-	public String description;
+	public String type;
+	public String location_name;
+	public String state;
 	public String background_image;
-	public int id;
-	public String end_time;
-	public String organizer_description;
+	public String timezone;
+	public String start_time;
+	public Call_for_papersModel _call_for_papers;
+	public CreatorModel _creator;
+	public String description;
 	public String schedule_published_on;
+	public CopyrightModel _copyright;
+	public String organizer_description;
+	public String email;
+	public ArrayList<Social_linksModel> social_links;
+	public String organizer_name;
+	public String topic;
 
-	public RootModel(String email, Call_for_papersModel call_for_papers, String state, CopyrightModel copyright, CreatorModel creator, String type, String start_time, ArrayList<Social_linksModel> social_links, String timezone, String location_name, String topic, VersionModel version, String name, String code_of_conduct, String organizer_name, String logo, String privacy, String description, String background_image, int id, String end_time, String organizer_description, String schedule_published_on) {
+	public RootModel(VersionModel version, String code_of_conduct, String name, String end_time, int id, String logo, String privacy, String type, String location_name, String state, String background_image, String timezone, String start_time, Call_for_papersModel call_for_papers, CreatorModel creator, String description, String schedule_published_on, CopyrightModel copyright, String organizer_description, String email, ArrayList<Social_linksModel> social_links, String organizer_name, String topic) {
 
-		this.email = email;
-		this._call_for_papers = call_for_papers;
-		this.state = state;
-		this._copyright = copyright;
-		this._creator = creator;
-		this.type = type;
-		this.start_time = start_time;
-		this.social_links = social_links;
-		this.timezone = timezone;
-		this.location_name = location_name;
-		this.topic = topic;
 		this._version = version;
-		this.name = name;
 		this.code_of_conduct = code_of_conduct;
-		this.organizer_name = organizer_name;
+		this.name = name;
+		this.end_time = end_time;
+		this.id = id;
 		this.logo = logo;
 		this.privacy = privacy;
-		this.description = description;
+		this.type = type;
+		this.location_name = location_name;
+		this.state = state;
 		this.background_image = background_image;
-		this.id = id;
-		this.end_time = end_time;
-		this.organizer_description = organizer_description;
+		this.timezone = timezone;
+		this.start_time = start_time;
+		this._call_for_papers = call_for_papers;
+		this._creator = creator;
+		this.description = description;
 		this.schedule_published_on = schedule_published_on;
+		this._copyright = copyright;
+		this.organizer_description = organizer_description;
+		this.email = email;
+		this.social_links = social_links;
+		this.organizer_name = organizer_name;
+		this.topic = topic;
 
 	}
 
