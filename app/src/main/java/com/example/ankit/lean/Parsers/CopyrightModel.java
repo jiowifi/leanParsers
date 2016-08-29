7d6df249793a74df3f434894143c651254c15a5e
jiowifi <xutephone16@gmail.com>
2016-08-29 22:06:39 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/CopyrightModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/CopyrightModel.java
@@ -2,18 +2,18 @@
 class CopyrightModel {
 
 	public String holder_url;
-	public int year;
 	public String logo;
 	public String licence_url;
+	public int year;
 	public String licence;
 	public String holder;
 
-	public CopyrightModel(String holder_url, int year, String logo, String licence_url, String licence, String holder) {
+	public CopyrightModel(String holder_url, String logo, String licence_url, int year, String licence, String holder) {
 
 		this.holder_url = holder_url;
-		this.year = year;
 		this.logo = logo;
 		this.licence_url = licence_url;
+		this.year = year;
 		this.licence = licence;
 		this.holder = holder;
 
--- app/src/main/java/com/example/ankit/lean/Parsers/RootModelParser.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModelParser.java
@@ -6,10 +6,10 @@ import org.json.JSONArray;
 class RootModelParser {
 
 		VersionModelParser version_parser = new VersionModelParser();
-		Call_for_papersModelParser call_for_papers_parser = new Call_for_papersModelParser();
-		CopyrightModelParser copyright_parser = new CopyrightModelParser();
-		CreatorModelParser creator_parser = new CreatorModelParser();
 		Social_linksModelParser social_links_parser;
+		Call_for_papersModelParser call_for_papers_parser = new Call_for_papersModelParser();
+		CreatorModelParser creator_parser = new CreatorModelParser();
+		CopyrightModelParser copyright_parser = new CopyrightModelParser();
 
 		public RootModelParser() {
 			social_links_parser = new Social_linksModelParser();
@@ -23,12 +23,6 @@ class RootModelParser {
 
 					VersionModel version = version_parser.parseVersionModel(jsobj.getJSONObject("version").toString());
 
-					Call_for_papersModel call_for_papers = call_for_papers_parser.parseCall_for_papersModel(jsobj.getJSONObject("call_for_papers").toString());
-
-					CopyrightModel copyright = copyright_parser.parseCopyrightModel(jsobj.getJSONObject("copyright").toString());
-
-					CreatorModel creator = creator_parser.parseCreatorModel(jsobj.getJSONObject("creator").toString());
-
 					ArrayList<Social_linksModel> social_linkss = new ArrayList<>();
 					JSONArray social_links_arr = jsobj.getJSONArray("social_links");
 			
@@ -38,7 +32,13 @@ class RootModelParser {
 
 					}
 
-					local_model = new RootModel(jsobj.getString("name") , jsobj.getString("end_time") , jsobj.getString("privacy") , version, jsobj.getString("email") , jsobj.getString("schedule_published_on") , jsobj.getString("type") , jsobj.getString("description") , jsobj.getString("state") , jsobj.getString("timezone") , jsobj.getInt("id") , jsobj.getString("organizer_description") , call_for_papers, jsobj.getString("background_image") , copyright, jsobj.getString("location_name") , jsobj.getString("organizer_name") , creator, jsobj.getString("logo") , jsobj.getString("start_time") , social_linkss, jsobj.getString("topic") , jsobj.getString("code_of_conduct") , );
+					Call_for_papersModel call_for_papers = call_for_papers_parser.parseCall_for_papersModel(jsobj.getJSONObject("call_for_papers").toString());
+
+					CreatorModel creator = creator_parser.parseCreatorModel(jsobj.getJSONObject("creator").toString());
+
+					CopyrightModel copyright = copyright_parser.parseCopyrightModel(jsobj.getJSONObject("copyright").toString());
+
+					local_model = new RootModel(jsobj.getString("logo") , jsobj.getString("name") , jsobj.getString("timezone") , jsobj.getString("organizer_name") , version, jsobj.getString("topic") , social_linkss, jsobj.getString("state") , jsobj.getString("email") , call_for_papers, jsobj.getString("type") , creator, jsobj.getInt("id") , jsobj.getString("code_of_conduct") , jsobj.getString("background_image") , jsobj.getString("end_time") , jsobj.getString("organizer_description") , jsobj.getString("location_name") , jsobj.getString("schedule_published_on") , jsobj.getString("privacy") , copyright, jsobj.getString("description") , jsobj.getString("start_time") , );
  			} 
 			catch (JSONException e){
 
