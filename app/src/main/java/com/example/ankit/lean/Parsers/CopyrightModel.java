3a69b9ff1abd0f6708421d1f6ba088ec71dcf510
jiowifi <xutephone16@gmail.com>
2016-08-29 22:14:33 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/CopyrightModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/CopyrightModel.java
@@ -2,20 +2,20 @@
 class CopyrightModel {
 
 	public String holder;
-	public String logo;
 	public String licence_url;
-	public String licence;
+	public String logo;
 	public String holder_url;
 	public int year;
+	public String licence;
 
-	public CopyrightModel(String holder, String logo, String licence_url, String licence, String holder_url, int year) {
+	public CopyrightModel(String holder, String licence_url, String logo, String holder_url, int year, String licence) {
 
 		this.holder = holder;
-		this.logo = logo;
 		this.licence_url = licence_url;
-		this.licence = licence;
+		this.logo = logo;
 		this.holder_url = holder_url;
 		this.year = year;
+		this.licence = licence;
 
 	}
 
