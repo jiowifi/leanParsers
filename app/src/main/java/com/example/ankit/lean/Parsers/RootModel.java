f93283c81da95b3bb588c122ca9042ef25adb471
jiowifi <xutephone16@gmail.com>
2016-08-29 22:13:31 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public String topic;
-	public String location_name;
-	public CopyrightModel _copyright;
-	public VersionModel _version;
-	public String start_time;
-	public String organizer_name;
-	public String schedule_published_on;
-	public String background_image;
-	public String logo;
-	public String code_of_conduct;
-	public String email;
-	public String description;
-	public int id;
-	public String state;
-	public CreatorModel _creator;
-	public String type;
-	public String organizer_description;
 	public Call_for_papersModel _call_for_papers;
-	public ArrayList<Social_linksModel> social_links;
-	public String end_time;
-	public String name;
-	public String timezone;
+	public String start_time;
+	public String type;
+	public String description;
+	public CopyrightModel _copyright;
 	public String privacy;
+	public String name;
+	public String location_name;
+	public String email;
+	public String end_time;
+	public String schedule_published_on;
+	public String organizer_name;
+	public String code_of_conduct;
+	public String timezone;
+	public String organizer_description;
+	public String topic;
+	public VersionModel _version;
+	public String state;
+	public ArrayList<Social_linksModel> social_links;
+	public CreatorModel _creator;
+	public String logo;
+	public String background_image;
+	public int id;
 
-	public RootModel(String topic, String location_name, CopyrightModel copyright, VersionModel version, String start_time, String organizer_name, String schedule_published_on, String background_image, String logo, String code_of_conduct, String email, String description, int id, String state, CreatorModel creator, String type, String organizer_description, Call_for_papersModel call_for_papers, ArrayList<Social_linksModel> social_links, String end_time, String name, String timezone, String privacy) {
+	public RootModel(Call_for_papersModel call_for_papers, String start_time, String type, String description, CopyrightModel copyright, String privacy, String name, String location_name, String email, String end_time, String schedule_published_on, String organizer_name, String code_of_conduct, String timezone, String organizer_description, String topic, VersionModel version, String state, ArrayList<Social_linksModel> social_links, CreatorModel creator, String logo, String background_image, int id) {
 
-		this.topic = topic;
-		this.location_name = location_name;
-		this._copyright = copyright;
-		this._version = version;
-		this.start_time = start_time;
-		this.organizer_name = organizer_name;
-		this.schedule_published_on = schedule_published_on;
-		this.background_image = background_image;
-		this.logo = logo;
-		this.code_of_conduct = code_of_conduct;
-		this.email = email;
-		this.description = description;
-		this.id = id;
-		this.state = state;
-		this._creator = creator;
-		this.type = type;
-		this.organizer_description = organizer_description;
 		this._call_for_papers = call_for_papers;
-		this.social_links = social_links;
-		this.end_time = end_time;
-		this.name = name;
-		this.timezone = timezone;
+		this.start_time = start_time;
+		this.type = type;
+		this.description = description;
+		this._copyright = copyright;
 		this.privacy = privacy;
+		this.name = name;
+		this.location_name = location_name;
+		this.email = email;
+		this.end_time = end_time;
+		this.schedule_published_on = schedule_published_on;
+		this.organizer_name = organizer_name;
+		this.code_of_conduct = code_of_conduct;
+		this.timezone = timezone;
+		this.organizer_description = organizer_description;
+		this.topic = topic;
+		this._version = version;
+		this.state = state;
+		this.social_links = social_links;
+		this._creator = creator;
+		this.logo = logo;
+		this.background_image = background_image;
+		this.id = id;
 
 	}
 
