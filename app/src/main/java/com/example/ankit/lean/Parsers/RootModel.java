14220038596ea438e9bdfc798204ec074effcf6d
jiowifi <xutephone16@gmail.com>
2016-08-29 22:13:40 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
-	public int id;
-	public String end_time;
-	public String privacy;
 	public String type;
-	public CreatorModel _creator;
-	public VersionModel _version;
-	public String email;
+	public String logo;
+	public String end_time;
+	public CopyrightModel _copyright;
+	public ArrayList<Social_linksModel> social_links;
+	public Call_for_papersModel _call_for_papers;
+	public String state;
 	public String organizer_description;
 	public String code_of_conduct;
 	public String start_time;
-	public String name;
-	public String timezone;
-	public String organizer_name;
-	public ArrayList<Social_linksModel> social_links;
-	public Call_for_papersModel _call_for_papers;
-	public String topic;
-	public String state;
-	public String schedule_published_on;
-	public String logo;
-	public String description;
-	public String background_image;
-	public CopyrightModel _copyright;
+	public VersionModel _version;
 	public String location_name;
+	public String timezone;
+	public String schedule_published_on;
+	public String email;
+	public String privacy;
+	public String organizer_name;
+	public String topic;
+	public String description;
+	public int id;
+	public String name;
+	public String background_image;
+	public CreatorModel _creator;
 
-	public RootModel(int id, String end_time, String privacy, String type, CreatorModel creator, VersionModel version, String email, String organizer_description, String code_of_conduct, String start_time, String name, String timezone, String organizer_name, ArrayList<Social_linksModel> social_links, Call_for_papersModel call_for_papers, String topic, String state, String schedule_published_on, String logo, String description, String background_image, CopyrightModel copyright, String location_name) {
+	public RootModel(String type, String logo, String end_time, CopyrightModel copyright, ArrayList<Social_linksModel> social_links, Call_for_papersModel call_for_papers, String state, String organizer_description, String code_of_conduct, String start_time, VersionModel version, String location_name, String timezone, String schedule_published_on, String email, String privacy, String organizer_name, String topic, String description, int id, String name, String background_image, CreatorModel creator) {
 
-		this.id = id;
-		this.end_time = end_time;
-		this.privacy = privacy;
 		this.type = type;
-		this._creator = creator;
-		this._version = version;
-		this.email = email;
+		this.logo = logo;
+		this.end_time = end_time;
+		this._copyright = copyright;
+		this.social_links = social_links;
+		this._call_for_papers = call_for_papers;
+		this.state = state;
 		this.organizer_description = organizer_description;
 		this.code_of_conduct = code_of_conduct;
 		this.start_time = start_time;
-		this.name = name;
-		this.timezone = timezone;
-		this.organizer_name = organizer_name;
-		this.social_links = social_links;
-		this._call_for_papers = call_for_papers;
-		this.topic = topic;
-		this.state = state;
-		this.schedule_published_on = schedule_published_on;
-		this.logo = logo;
-		this.description = description;
-		this.background_image = background_image;
-		this._copyright = copyright;
+		this._version = version;
 		this.location_name = location_name;
+		this.timezone = timezone;
+		this.schedule_published_on = schedule_published_on;
+		this.email = email;
+		this.privacy = privacy;
+		this.organizer_name = organizer_name;
+		this.topic = topic;
+		this.description = description;
+		this.id = id;
+		this.name = name;
+		this.background_image = background_image;
+		this._creator = creator;
 
 	}
 
--- app/src/main/java/com/example/ankit/lean/Parsers/RootModelParser.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModelParser.java
@@ -5,11 +5,11 @@ import org.json.JSONArray;
 
 class RootModelParser {
 
-		CreatorModelParser creator_parser = new CreatorModelParser();
-		VersionModelParser version_parser = new VersionModelParser();
+		CopyrightModelParser copyright_parser = new CopyrightModelParser();
 		Social_linksModelParser social_links_parser;
 		Call_for_papersModelParser call_for_papers_parser = new Call_for_papersModelParser();
-		CopyrightModelParser copyright_parser = new CopyrightModelParser();
+		VersionModelParser version_parser = new VersionModelParser();
+		CreatorModelParser creator_parser = new CreatorModelParser();
 
 		public RootModelParser() {
 			social_links_parser = new Social_linksModelParser();
@@ -21,9 +21,7 @@ class RootModelParser {
 			try {
 					JSONObject jsobj = new JSONObject(json_object);
 
-					CreatorModel creator = creator_parser.parseCreatorModel(jsobj.getJSONObject("creator").toString());
-
-					VersionModel version = version_parser.parseVersionModel(jsobj.getJSONObject("version").toString());
+					CopyrightModel copyright = copyright_parser.parseCopyrightModel(jsobj.getJSONObject("copyright").toString());
 
 					ArrayList<Social_linksModel> social_linkss = new ArrayList<>();
 					JSONArray social_links_arr = jsobj.getJSONArray("social_links");
@@ -36,9 +34,11 @@ class RootModelParser {
 
 					Call_for_papersModel call_for_papers = call_for_papers_parser.parseCall_for_papersModel(jsobj.getJSONObject("call_for_papers").toString());
 
-					CopyrightModel copyright = copyright_parser.parseCopyrightModel(jsobj.getJSONObject("copyright").toString());
+					VersionModel version = version_parser.parseVersionModel(jsobj.getJSONObject("version").toString());
 
-					local_model = new RootModel(jsobj.getInt("id") , jsobj.getString("end_time") , jsobj.getString("privacy") , jsobj.getString("type") , creator, version, jsobj.getString("email") , jsobj.getString("organizer_description") , jsobj.getString("code_of_conduct") , jsobj.getString("start_time") , jsobj.getString("name") , jsobj.getString("timezone") , jsobj.getString("organizer_name") , social_linkss, call_for_papers, jsobj.getString("topic") , jsobj.getString("state") , jsobj.getString("schedule_published_on") , jsobj.getString("logo") , jsobj.getString("description") , jsobj.getString("background_image") , copyright, jsobj.getString("location_name") , );
+					CreatorModel creator = creator_parser.parseCreatorModel(jsobj.getJSONObject("creator").toString());
+
+					local_model = new RootModel(jsobj.getString("type") , jsobj.getString("logo") , jsobj.getString("end_time") , copyright, social_linkss, call_for_papers, jsobj.getString("state") , jsobj.getString("organizer_description") , jsobj.getString("code_of_conduct") , jsobj.getString("start_time") , version, jsobj.getString("location_name") , jsobj.getString("timezone") , jsobj.getString("schedule_published_on") , jsobj.getString("email") , jsobj.getString("privacy") , jsobj.getString("organizer_name") , jsobj.getString("topic") , jsobj.getString("description") , jsobj.getInt("id") , jsobj.getString("name") , jsobj.getString("background_image") , creator, );
  			} 
 			catch (JSONException e){
 
