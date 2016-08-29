2dfe1c0daed89d6b11e2641dedcd24a1993ca3c8
jiowifi <xutephone16@gmail.com>
2016-08-29 22:14:00 +0530

    retrying to push failed one

--- app/src/main/java/com/example/ankit/lean/Parsers/CopyrightModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/CopyrightModel.java
@@ -1,20 +1,20 @@
 
 class CopyrightModel {
 
-	public String logo;
-	public String holder_url;
+	public int year;
 	public String holder;
 	public String licence;
-	public int year;
+	public String logo;
+	public String holder_url;
 	public String licence_url;
 
-	public CopyrightModel(String logo, String holder_url, String holder, String licence, int year, String licence_url) {
+	public CopyrightModel(int year, String holder, String licence, String logo, String holder_url, String licence_url) {
 
-		this.logo = logo;
-		this.holder_url = holder_url;
+		this.year = year;
 		this.holder = holder;
 		this.licence = licence;
-		this.year = year;
+		this.logo = logo;
+		this.holder_url = holder_url;
 		this.licence_url = licence_url;
 
 	}
--- app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
+++ app/src/main/java/com/example/ankit/lean/Parsers/RootModel.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 class RootModel {
 
+	public String organizer_description;
+	public String start_time;
+	public String privacy;
 	public String location_name;
+	public String background_image;
+	public ArrayList<Social_linksModel> social_links;
+	public String email;
+	public String name;
 	public String logo;
 	public String type;
-	public VersionModel _version;
-	public String background_image;
 	public Call_for_papersModel _call_for_papers;
-	public CreatorModel _creator;
-	public String topic;
-	public String state;
-	public int id;
-	public String privacy;
-	public String description;
+	public VersionModel _version;
 	public CopyrightModel _copyright;
-	public String name;
-	public String end_time;
-	public String email;
-	public String start_time;
-	public String code_of_conduct;
-	public String organizer_name;
+	public String state;
+	public String topic;
 	public String schedule_published_on;
+	public String code_of_conduct;
 	public String timezone;
-	public String organizer_description;
-	public ArrayList<Social_linksModel> social_links;
+	public String organizer_name;
+	public CreatorModel _creator;
+	public int id;
+	public String end_time;
+	public String description;
 
-	public RootModel(String location_name, String logo, String type, VersionModel version, String background_image, Call_for_papersModel call_for_papers, CreatorModel creator, String topic, String state, int id, String privacy, String description, CopyrightModel copyright, String name, String end_time, String email, String start_time, String code_of_conduct, String organizer_name, String schedule_published_on, String timezone, String organizer_description, ArrayList<Social_linksModel> social_links) {
+	public RootModel(String organizer_description, String start_time, String privacy, String location_name, String background_image, ArrayList<Social_linksModel> social_links, String email, String name, String logo, String type, Call_for_papersModel call_for_papers, VersionModel version, CopyrightModel copyright, String state, String topic, String schedule_published_on, String code_of_conduct, String timezone, String organizer_name, CreatorModel creator, int id, String end_time, String description) {
 
+		this.organizer_description = organizer_description;
+		this.start_time = start_time;
+		this.privacy = privacy;
 		this.location_name = location_name;
+		this.background_image = background_image;
+		this.social_links = social_links;
+		this.email = email;
+		this.name = name;
 		this.logo = logo;
 		this.type = type;
-		this._version = version;
-		this.background_image = background_image;
 		this._call_for_papers = call_for_papers;
-		this._creator = creator;
-		this.topic = topic;
-		this.state = state;
-		this.id = id;
-		this.privacy = privacy;
-		this.description = description;
+		this._version = version;
 		this._copyright = copyright;
-		this.name = name;
-		this.end_time = end_time;
-		this.email = email;
-		this.start_time = start_time;
-		this.code_of_conduct = code_of_conduct;
-		this.organizer_name = organizer_name;
+		this.state = state;
+		this.topic = topic;
 		this.schedule_published_on = schedule_published_on;
+		this.code_of_conduct = code_of_conduct;
 		this.timezone = timezone;
-		this.organizer_description = organizer_description;
-		this.social_links = social_links;
+		this.organizer_name = organizer_name;
+		this._creator = creator;
+		this.id = id;
+		this.end_time = end_time;
+		this.description = description;
 
 	}
 
